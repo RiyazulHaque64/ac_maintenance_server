372ecf1915e4486aab4ec1e79580e12990ce05d5
riyazulhaque64 <riyazulhaque64@gmail.com>
2026-02-28 17:47:49 +0600

    update

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,8 @@ app.use((req, res, next) => {
     "https://maintenance-client-smoky.vercel.app",
   );
 
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS",
