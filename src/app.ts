ca5b3a9636795d063f485614d2b26c3a06ef4005
riyazulhaque64 <riyazulhaque64@gmail.com>
2026-02-28 13:54:33 +0600

    update  cors origin

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,22 @@ import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
+const allowedOrigins = ["http://localhost:3000"];
+
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
